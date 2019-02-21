ef0cd1ba1c8c0c4b4905673ff81e152b90740e5b
linhnk242 <47785633+linhnk242@users.noreply.github.com>
2019-02-21 15:38:20 +0700

    Add files via upload
    Homework Session 1

---
+++ Homework1.py
@@ -0,0 +1,18 @@
+message = "Hello, World!"
+print(type(message))
+x = 3
+print(type(x))
+pi = 3.14159
+print(type(pi))
+
+# not begin with a letter
+24days = "best"
+print(24days)
+
+# contain an illegal character
+add$ = 200
+print(add$)
+
+# variable name = Python keywords
+def = "linh"
+print(def)
